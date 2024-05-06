e4fdbde5857ca3d0eb3822a36d704233921cc1c9
RohanCBhavsar <91047108+RohanCBhavsar@users.noreply.github.com>
2024-05-06 12:18:14 +0530

    update creds

--- MEWS_client/env.ts
+++ MEWS_client/env.ts
@@ -2,5 +2,5 @@ export const clientToken =
   "9381AB282F844CD9A2F4AD200158E7BC-D27113FA792B0855F87D0F93E9E1D71";
 export const accessToken =
   "471DA6962614405D9FF9AD6E0086AE99-3F2BC8F2F68D568AA3B72E402BB80DF";
-export const clientName = "Sample Client 1.0.0";
+export const clientName = "Rate Quantum";
 export const baseUrl = "https://api.mews-demo.com/api/connector/v1";
